0cd730ad558d9cc836b22d4e2aa63166433e1937
John Forte <fortejohn@gmail.com>
2023-02-19 23:00:04 -0500

    fix: make variables more uniform

--- starship.py
+++ starship.py
@@ -13,21 +13,21 @@ def read_docker_compose(file):
     return doc
 
 
-configFile = ""
+config_file = ""
 
 if os.path.isfile("starship.config.toml"):
-    configFile = "starship.config.toml"
+    config_file = "starship.config.toml"
 elif Path.home().joinpath('.starship.config.toml').is_file():
-    configFile = Path.home().joinpath('.starship.config.tom')
+    config_file = Path.home().joinpath('.starship.config.tom')
 
-config = toml.load(configFile)
+config = toml.load(config_file)
 
-repoPath = Path(config['git']['dir_to_clone'])
+repo_path = Path(config['git']['dir_to_clone'])
 
-if not repoPath.joinpath('.git').is_dir():
+if not repo_path.joinpath('.git').is_dir():
     git.clone(config['git']['repo'], config['git']['dir_to_clone'])
 else:
-    git.pull(_cwd=repoPath.absolute())
+    git.pull(_cwd=repo_path.absolute())
 
 
 if config['registery']['private']:
@@ -37,13 +37,13 @@ if config['registery']['private']:
         config['registery']['password'],
     )
 
-compose = read_docker_compose(repoPath.joinpath(config['compose']['file']))
+compose = read_docker_compose(repo_path.joinpath(config['compose']['file']))
 
 if config['compose']['force_repull']:
     for key in compose['services']:
         docker.pull(compose['services'][key]['image'])
 
-docker = DockerClient(compose_files=[repoPath.joinpath(config['compose']['file'])])
+docker = DockerClient(compose_files=[repo_path.joinpath(config['compose']['file'])])
 
 docker.compose.up(
     force_recreate=True,
